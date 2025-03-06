73be3bea8ebf4de2bab78fe3500dbe7cad6feedd
Star7-jpg <marcosconde612@gmail.com>
2025-03-06 12:56:57 -0600

    Inbox page (static)

---
+++ app/components/inbox/Conversation.tsx
@@ -0,0 +1,12 @@
+const Conversartion = () => {
+    return(
+        <div className="px-6 py-4 cursor-pointer border border-gray-3000 rounded-xl">
+            <p className="mb-6 text-xl ">John Doe</p>
+
+            <p className="text-airbnb-dark">Go to Conversartion </p>
+
+        </div>
+    )
+}
+
+export default Conversartion;
