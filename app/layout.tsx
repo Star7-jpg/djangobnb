79c8c85aba6a84406b5761a45daa3d9a64fdb3da
Star7-jpg <marcosconde612@gmail.com>
2025-03-10 21:10:10 -0600

    12 Create reusable model windows

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from './components/navbar/Navbar'
+import Modal from "./components/modals/Modal";
 
 
 const geistSans = Geist({
@@ -24,6 +25,11 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const content = (
+      <p>Yojdjldsj</p>
+    )
+
+  
   return (
     <html lang="es">
       <body
